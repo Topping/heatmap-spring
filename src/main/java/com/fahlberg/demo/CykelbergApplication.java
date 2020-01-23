76aa733111369fe5700796d86e2a85188ec03480
Fahlberg <Fahlberg@live.dk>
2020-01-23 02:28:58 +0100

    component scan

--- src/main/java/com/fahlberg/demo/CykelbergApplication.java
+++ src/main/java/com/fahlberg/demo/CykelbergApplication.java
@@ -2,9 +2,12 @@ package com.fahlberg.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.fahlberg"})
 public class CykelbergApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CykelbergApplication.class, args);
